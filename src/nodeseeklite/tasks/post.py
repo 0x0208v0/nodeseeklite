55ca3598feca455379e2dc0aad2af01a8e00693d
0x0208 Version 0 <70836233+0x0208v0@users.noreply.github.com>
2024-05-25 23:56:32 +0800

    update

--- src/nodeseeklite/tasks/post.py
+++ src/nodeseeklite/tasks/post.py
@@ -5,14 +5,18 @@ logger = logging.getLogger(__name__)
 
 def crawl_post():
     logger.info(f'crawl_post start!')
+    try:
 
-    from nodeseeklite.app import app
-    from nodeseeklite.models import db
-    from nodeseeklite.models.post import Post
+        from nodeseeklite.app import app
+        from nodeseeklite.models import db
+        from nodeseeklite.models.post import Post
 
-    with app.app_context():
-        Post.crawl()
-        db.session.close_all()
+        with app.app_context():
+            Post.crawl()
+            db.session.close_all()
+
+    except Exception as e:
+        logger.error(f'{e}')
 
     logger.info(f'crawl_post done!')
 
